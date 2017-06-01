9146313afccf2543366c9a472ea9193a25f2ed22
Joshua Simon <Joshua.Simon@dlr.de>
2017-06-01 15:31:59 +0200

    the places stuff

--- angular-map/src/app/app.component.ts
+++ angular-map/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+
+import { ElementRef, NgZone, OnInit, ViewChild, Component } from '@angular/core';
+import { FormControl } from "@angular/forms";
+import { MapsAPILoader } from 'angular2-google-maps/core'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+ 
+  public latitude: number;
+  public longitude: number;
+  public searchControl: FormControl;
+  public zoom: number;
+
+  @ViewChild("search")
+  public searchElementRef: ElementRef;
+
+  constructor(
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
+  ) {}
+
+  ngOnInit() {
+    //set google maps defaults
+    this.zoom = 4;
+    this.latitude = 39.8282;
+    this.longitude = -98.5795;
+
+    //create search FormControl
+    this.searchControl = new FormControl();
+
+    //set current position
+
+    //load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ["address"]
+      });
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          //set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 12;
+          console.log(this.latitude + this.longitude);
+        });
+      });
+    });
+  }
+
+ 
+  
+  
 }
