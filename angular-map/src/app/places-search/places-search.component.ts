d6e426fe84973625a13abb642708c722aba02986
Joshua Simon <Joshua.Simon@dlr.de>
2017-06-02 12:03:32 +0200

    lÃ¶ast

--- angular-map/src/app/places-search/places-search.component.ts
+++ angular-map/src/app/places-search/places-search.component.ts
@@ -28,7 +28,7 @@ export class PlacesSearchComponent implements OnInit {
   ngOnInit(){
   this.mapsAPILoader.load().then(() => {
       let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
-        types: ["address"]
+        types: ["geocode"]
       });
       autocomplete.addListener("place_changed", () => {
         this.ngZone.run(() => {
